f0aba2e356d60002454ae95c16060f21fb58fefb
Elliot Coyle <ejcoyle88@users.noreply.github.com>
2020-07-29 12:46:16 +0100

    Delete createLinks.sh

--- createLinks.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-SRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-#SRC=$(dirname "$BASH_SOURCE")
-
-ln -sf $SRC/.vimrc ~/.vimrc
-ln -sf $SRC/.vim ~/.vim
-
-ln -sf $SRC/.tmux.conf ~/.tmux.conf
-ln -sf $SRC/.tmux ~/.tmux

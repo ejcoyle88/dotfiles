c759ec490d4a39302fe6f8c8fb02e5323d52ca98
Elliot Coyle <ejcoyle88@users.noreply.github.com>
2020-07-29 12:46:09 +0100

    Delete linkConfig.ps1

--- linkConfig.ps1
+++
@@ -1,6 +0,0 @@
-$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-
-mklink %HOMEPATH%\.vimrc $scriptPath\.vimrc
-mklink %HOMEPATH%\.vim $scriptPath\.vim
-mklink %HOMEPATH%\.tmux.config $scriptPath\.tmux.config
-mklink %HOMEPATH%\.tmux $scriptPath\.tmux

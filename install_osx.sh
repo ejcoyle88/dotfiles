026e0e8a9a1be66e638796386e5fe134dd7366b7
Elliot Coyle <ejcoyle88@users.noreply.github.com>
2022-04-04 21:40:42 +0100

    Adding osx config

--- install_osx.sh
+++ install_osx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CONFIG="install.osx.conf.yaml"
+CONFIG="install.osx.config.yaml"
 
 set -e
 
@@ -13,3 +13,22 @@ git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
 git submodule update --init --recursive "${DOTBOT_DIR}"
 
 "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install git
+brew install --cask postman
+brew install --cask jetbrains-toolbox
+brew install --cask azure-data-studio
+brew install --cask dotnet-sdk
+brew install neovim
+brew install nvm
+brew install pyenv
+
+brew tap homebrew/cask-fonts
+brew install --cask font-fira-code
+brew install --cask font-fira-mono-nerd-font
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+py -m pip install pynvim

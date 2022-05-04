bd6be42777df164aabc67a86e27b098459daee91
Elliot Coyle <ejcoyle88@users.noreply.github.com>
2022-05-04 19:48:42 +0100

    Getting dotbot-brew working locally

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-PLUGINS=""
+git submodule update --init --recursive 
+
+set -e
+
 if [[ "$OSTYPE" == "msys" ]]; then
     if ! [[ $(sfc 2>&1 | tr -d '\0') =~ SCANNOW ]]; then
         echo "This command must be run as an administrator";
@@ -11,22 +14,11 @@ if [[ "$OSTYPE" == "msys" ]]; then
     CONFIG="install.win.conf.yaml"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     CONFIG="install.osx.conf.yaml"
-
-    PLUGINS="--plugin-dir dotbot-brew"
-    git -C "dotbot-brew" submodule sync --quiet --recursive
-    git submodule update --init --recursive "dotbot-brew"
+    ./dotbot/bin/dotbot -d . --plugin-dir "dotbot-brew" -c "${CONFIG}" "${@}" -v
+    exit 1
 else
     CONFIG="install.conf.yaml"
 fi
 
-set -e
 
-DOTBOT_DIR="dotbot"
-DOTBOT_BIN="bin/dotbot"
-BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-cd "${BASEDIR}"
-git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
-git submodule update --init --recursive "${DOTBOT_DIR}"
-
-"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" ${PLUGINS} -c "${CONFIG}" "${@}"
+./dotbot/bin/dotbot -d . -c "${CONFIG}" "${@}" -v

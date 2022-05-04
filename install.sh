32876fd64ca3cae85a9fd5b779711459779aa344
Elliot Coyle <ejcoyle88@users.noreply.github.com>
2022-05-04 18:28:10 +0100

    Using dotbot-brew to speed up Homebrew

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+PLUGINS=""
 if [[ "$OSTYPE" == "msys" ]]; then
     if ! [[ $(sfc 2>&1 | tr -d '\0') =~ SCANNOW ]]; then
         echo "This command must be run as an administrator";
@@ -10,6 +11,7 @@ if [[ "$OSTYPE" == "msys" ]]; then
     CONFIG="install.win.conf.yaml"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     CONFIG="install.osx.conf.yaml"
+    PLUGINS="--plugin-dir dotbot-brew"
 else
     CONFIG="install.conf.yaml"
 fi
@@ -24,4 +26,4 @@ cd "${BASEDIR}"
 git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
 git submodule update --init --recursive "${DOTBOT_DIR}"
 
-"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
+"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" ${PLUGINS} -c "${CONFIG}" "${@}"
